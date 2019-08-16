fb03624bf9999d0be86a09b263587bb045b34c79
zrysnd <503591415@qq.com>
2019-08-16 12:02:13 -0400

    javadoc

--- src/Connect/ServerMultiTCPConnection.java
+++ src/Connect/ServerMultiTCPConnection.java
@@ -7,7 +7,7 @@ import java.util.List;
 import client.Models.ItemContainer;
 
 /**
- * 
+ * This class is used to group all server's network resources together.
  * @author rongyi
  * @version 1.0
  *
@@ -26,7 +26,7 @@ public class ServerMultiTCPConnection {
 	private boolean ready_;
 	
 	/**
-	 * 
+	 * Constructs a instance of this class given number of players and a port number.
 	 * @param numberOfClientsWanted : the number of players in this game.
 	 * @param portNumber : Port number this server will use
 	 * @throws IOException: network related.
@@ -42,6 +42,10 @@ public class ServerMultiTCPConnection {
 		
 	}
 	
+	/**
+	 * Wait for all clients to connect.
+	 * @throws IOException If connection fails
+	 */
 	public void connect() throws IOException {
 		for( int i = 0; i < this.numberOfClientsWanted_; i++) {
 			this.sockets_.add( this.serverSocket_.accept() );
@@ -59,19 +63,40 @@ public class ServerMultiTCPConnection {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param i the index of a inputStream, each inputStream corresponds to a client.
+	 * @return the input stream so that other classes can use it to read data from clients.
+	 */
 	public DataInputStream getInputStream(int i) {
 		return this.inputs_.get(i);
 	}
 	
+	/**
+	 * 
+	 * @param i the index of a outputStream, each outputStream corresponds to a client.
+	 * @return the outputStream so that other classes can use it to send data to clients.
+	 */
 	public ObjectOutputStream getOutputStream(int i) {
 		return this.outputs_.get(i);
 	}
 	
+	/**
+	 * 
+	 * @param i the index of the client to read data from. 
+	 * @return the integer read from a given client
+	 * @throws IOException usually when the i-th client disconnects.
+	 */
 	public int receive(int i) throws IOException {
 		int signal = this.inputs_.get(i).readInt();
 		return signal;
 	}
 	
+	/**
+	 * Send the same data to all clients.
+	 * @param Movings, an ItemContainer containing all display related data for all movable
+	 * objects in this game.
+	 */
 	public void sendToAll(ItemContainer Movings){
 		
 			for( int i = 0; i < this.numberOfClientsWanted_; i++) {
@@ -101,6 +126,10 @@ public class ServerMultiTCPConnection {
 	
 	//just mark the client as disconnected, so that the list of streams is untouched,
 	//otherwise have to update clients' ids.
+	/**
+	 * Marks a client as disconnected after it disconnected.
+	 * @param i the index of the client just disconnected from server.
+	 */
 	public void oneClientDisconnected( int i ) {
 //		this.inputs_.remove(i);
 //		this.outputs_.remove(i);
@@ -110,10 +139,17 @@ public class ServerMultiTCPConnection {
 		this.numberOfActiveClients_ --;
 	}
 	
+	/**
+	 * 
+	 * @return the number of currently active clients 
+	 */
 	public int getNofActive() {
 		return this.numberOfActiveClients_;
 	}
 	
+	/**
+	 * close socket and realise resouces.
+	 */
 	public void closeSocket() {
 		try {
 			this.serverSocket_.close();
