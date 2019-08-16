04cb74ea2ada0c8b3cc7a5ea4bf00c70f1c6cab2
formycat <wangj39@rpi.edu>
2019-08-15 20:23:54 -0400

    hhhh

--- src/client/Models/ServerSimulator.java
+++ src/client/Models/ServerSimulator.java
@@ -9,16 +9,18 @@ import java.util.TimerTask;
 
 public class ServerSimulator {
 	private int[][] map;
-	private ArrayList<Tank> tanks;
+	private List<Tank> tanks;
 	
-	private static final int MAP_WIDTH = 2000;
-	private static final int MAP_HEIGTH = 2000;
+	private int MAP_WIDTH ;
+	private int MAP_HEIGTH ;
+	
 	private static final int WALL_SIZE = 20;
 	
 	
 	public ServerSimulator(int[][] m, ArrayList<Tank> t) {
 		map = m;
 		tanks = t;
+		MAP_WIDTH = map[0].length*WALL_SIZE;
 		
 	}
 	
@@ -27,6 +29,24 @@ public class ServerSimulator {
 			tank.box.x+= tank.dx;
 			tank.box.y+=tank.dy;
 			//
+			
+			if(tank.box.x<0 || tank.box.x>MAP_WIDTH) tank.box.x-=tank.dx;
+			if(tank.box.y<0 || tank.box.y>MAP_HEIGTH) tank.box.y-=tank.dy;
+			
+			int row = (int)(tank.box.y)/WALL_SIZE;
+			int col = (int)(tank.box.x)/WALL_SIZE;
+			int row2 = (int)(tank.box.y+tank.size)/WALL_SIZE;
+			int col2 = (int)(tank.box.x+tank.size)/WALL_SIZE;
+			
+			int maxRow = map.length - 1;
+			int maxCol = map[0].length - 1;
+			if(row2 > maxRow || col2 > maxRow) {
+				if(row2>maxRow+2||col2>maxCol+3) {
+					tank.box.x -= tank.dx;
+					tank.box.y -= tank.dy;
+				}
+			}
+			
 			
 			
 			
